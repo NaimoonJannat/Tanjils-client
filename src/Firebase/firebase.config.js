0483fb01cdddf84c73bade1f57f217877a9fd6d6
NaimoonJannat <naimoonjannat@gmail.com>
2026-01-13 00:53:01 +0600

    first commit

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,11 +5,11 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_apiKey,
-    authDomain: import.meta.env.VITE_authDomain,
-    projectId: import.meta.env.VITE_projectId,
-    storageBucket: import.meta.env.VITE_storageBucket,
-    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGESENDERID,
     appId: import.meta.env.VITE_appId
 };
 
