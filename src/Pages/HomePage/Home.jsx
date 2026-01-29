edc4a0458213dec1f2e174fc379a1702a11ac485
NaimoonJannat <naimoonjannat@gmail.com>
2026-01-29 18:58:22 +0600

    theme fixed

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -3,6 +3,15 @@ const Home = () => {
     return (
         <div>
            <h2 className="text-5xl">This is Home</h2>
+          <div className="bg-primary text-tertiary">
+  Doctor Profile
+</div>
+
+<button className="bg-secondary text-primary">
+  Book Appointment
+</button>
+
+
         </div>
     );
 };
