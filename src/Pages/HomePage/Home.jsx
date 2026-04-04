2d24e3ba78d25660c9344bee63441bb1829063e1
NaimoonJannat <naimoonjannat@gmail.com>
2026-04-04 12:51:56 +0600

    Videosection Created

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,6 +1,7 @@
 import AboutMe from "./AboutMe";
 import AppointmentSection from "./AppointmentSection";
 import HeroBanner from "./HeroBanner";
+import VideoSection from "./VideoSection";
 import TreatmentSection from "./TreatmentSection";
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
           <HeroBanner></HeroBanner>
           <AboutMe></AboutMe>
           <TreatmentSection></TreatmentSection>
+          <VideoSection></VideoSection>
           <AppointmentSection></AppointmentSection>
         </div>
     );
